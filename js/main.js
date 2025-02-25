1bc06539853f0ec3c83a07a3dfce781109bcc1d7
Mandeep Masoun <mandeepmasoun@yahoo.com>
2025-02-24 22:30:29 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+//console.log('what us up gangg');
+//alert("ay say gang");
+//document.write("SKI")
+//process.stdout.write("jonkler");
+
+tree = () => {
+    const length = 10;
+    let output = '';
+
+    output += line(length);
+
+    for (let size = length; size > 0; size--) {
+
+        let star = length - size;
+        let space = length - star - 1;
+
+        for (let i = 0; i < space + 1; i++) {
+            output += " ";
+        }
+        for (let i = 0; i < star + 1; i++) {
+            output += "*";
+        }
+        for (let i = 0; i < star; i++) {
+            output += "*";
+        }
+        for (let i = 0; i < space; i++) {
+            output += " ";
+        }
+
+        output += "\t\t" + "space: " + space + " star: " + star + " sum: " + (space + star) + " i: " + (10 - size + 1) + "\n";
+    }
+    return output + line(length);
+}
+line = (num) => {
+    let out = "";
+    for (let i = 0; i < (2 * num) + 1; i++) {
+        out += "-"
+    }
+    out += "\n";
+    return out;
+}
+
+console.log(tree());
